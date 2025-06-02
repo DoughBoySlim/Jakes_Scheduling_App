68d0bc1371ec0ca31100bd6fc52f78dc6f0431b2
DoughBoySlim <rx5857yk@go.minnstate.edu>
2025-06-02 14:33:10 -0500

    Got the footer header to look like the figma design. Going to work on setting the logo, and sign in capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,32 @@ function App() {
 
   return (
     <>
-      <div className="sign-up-header">
-        <h1>The Ultimate Scheduling Application</h1>
+    <div className='min-h-screen flex flex-col'>
+      <main className='flex-grow'>
+      <div className="w-full bg-[#ad4c4c] text-[white] py-1 font-display">
+        <h1 className="text-3xl/25 text-center ">Welcome To The Jake's Scheduling Application</h1>
       </div>
       
-      <div className='SignUpArea'>
-        <div className="logo">
+      <div className='static'>
+        <div class="inline-block top-0">
           <img src={reactLogo} alt='JakesLogo'></img>
         </div>
-        <div className='SignUp'>
+        <div className='absolute inline-block'>
           <h1 class="text-5x1 font-bold center"> Sign In </h1>
           <h3> Username </h3>
           <h3> Password </h3>
           <button className='Submit'>Submit</button>
         </div>
       </div>
+      </main>
 
-      <div className='footer'>
-        <h1>@CausticCreations</h1>
-      </div>
+      <footer className="bg-[#fc0301] w-full shadow-sm m-0">
+        <div className='w-full max-w-screen-xl mx-auto py-8 flex justify-center'>
+          <span className="text-center text-white text-xl font-display">@CausticCreations</span>
+        </div>
+      </footer>
+    </div>
+
 
     </>
   )
