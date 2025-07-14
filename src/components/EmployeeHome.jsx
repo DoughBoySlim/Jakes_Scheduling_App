7b294f45d2fc45f073f225bc78bc1d52de97be18
DoughBoySlim <rx5857yk@go.minnstate.edu>
2025-07-14 14:24:14 -0500

    Routes SetUp Correctly, Moving on to checking if supabase registers the input fields and logs me in

--- src/components/EmployeeHome.jsx
+++ src/components/EmployeeHome.jsx
@@ -1,5 +1,4 @@
-import './index.css'
-
+import '../index.css'
 function EmployeeHome() {
 
     return (
---
+++ src/components/ManagerHome.jsx
@@ -0,0 +1,11 @@
+
+
+function ManagerHome() {
+    return (
+        <>
+            <h1>This is the managers home</h1>
+        </>
+    )
+}
+
+export default ManagerHome;
