8368a81cc58539daf3e087b508a0ffeb7acb876a
DoughBoySlim <rx5857yk@go.minnstate.edu>
2025-07-14 15:57:54 -0500

    Created an authentication system using supabase. Now need to adjust the routing of pages when signed in

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,18 +1,33 @@
 import supabase from "../config/supabaseClient";
-import { useState } from 'react'
 
-const [errorMessage, setErrorMessage] = useState('');
-const [loggedIn, setLoggedIn] = useState(false);
 
+// Checking if the Username and Password are in the DB
 export async function signIn(email, password) {
     const { data, error } = await supabase.auth.signInWithPassword({
-        email: email,
-        password: password,
+        email,
+        password,
     });
-    if(error) {
-        setErrorMessage(error);
-    }
-    else {
-        setLoggedIn(true);
-    }
+
+    return { data, error };
+}
+
+export async function signUp(email, password) {
+    const { data, error } = await supabase.auth.signUp({
+        email,
+        password
+    });
+
+    return { data, error };
+}
+
+
+// Writing a function to check if the user is a manager from the DB column 'isManager'
+export async function isManager(username) {
+    const { data, error } = supabase
+    .from("users")
+    .select('isManager')
+    .eq('full_name', username)
+    .single();
+
+    return { data, error };
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,32 +1,68 @@
 import supabase from '../config/supabaseClient'
 import { useState } from 'react'
 import reactLogo from '../assets/JakesLogo_Revamped.png'
+import EmployeeHome from './EmployeeHome.jsx'
+import ManagerHome from './ManagerHome.jsx'
+import { signIn, signUp, isManager } from '../api/auth.js'
 import '../index.css'
 
 function MainPage() {
 
-    async function fetchUsers() {
-        const { data, error } = await supabase 
-            .from('users')
-            .select('*');
-        
-            if(error) {
-                console.error('Error fetching users:', error);
-            } else {
-                console.log('Users:', data);
-            }
-    }
+    const [email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const[errorMessage, setErrorMessage] = useState('');
+    const[userRole, setUserRole] = useState('');
     
+    async function onSubmitPress() {
+        const { data, error } = await signIn(email, password);
 
-    const [loggedIn, setLoggedIn] = useState(false);
-    function onSubmitPress() {
-        fetchUsers();
+        if(error) {
+            setErrorMessage(error.message);
+            alert(error);
+        }
+        else {
+            const { data: managerData, error: managerError} = await isManager(email);
+            if(managerError) {
+                setErrorMessage(managerError.message);
+            }
+            else {
+                if(managerData && managerData.isManager) {
+                    setUserRole('manager');
+                }
+                else {
+                    setUserRole('employee');
+                }
+            }
+        }
+    }
+
+    async function onSignUp() {
+        const { data, error } = await signUp(email, password);
+
+        if(!error) {
+            await supabase
+            .from('users')
+            .insert({
+                employee_id: id,
+                full_name: email,
+                isManager: false,
+                auth_id: data.user.id 
+            });
+        }
+        else {
+            setErrorMessage(error.message);
+            alert(error);
+        }
+
     }
 
     return (
       <>
       <div className='min-h-screen flex flex-col'>
         <main className='flex-grow'>
+            {/* Go to the users respective page */}
+            {userRole === 'manager' && <ManagerHome/>}
+            {userRole === 'employee' && <EmployeeHome/>}
         <div className="w-full bg-[#ad4c4c] text-[white] py-1 font-display">
           <h1 className="text-3xl/25 text-center ">Welcome To The Jake's Scheduling Application</h1>
         </div>
@@ -38,14 +74,15 @@ function MainPage() {
           <div className='basis-1/2 flex flex-col p-10 my-[6rem] '>
             <h1 className="text-4xl text-center text-[#fc0301] font-display"> Sign In </h1>
             <div className='flex flex-col items-start'>
-              <h3 className='text-md pt-5 font-display'> Username </h3>
-              <input className="bg-[#ad4c4c] rounded-sm w-full"></input>
+              <h3 className='text-md pt-5 font-display'> Employee Id </h3>
+              <input className="bg-[#ad4c4c] rounded-sm w-full" onChange={e => setEmail(e.target.value)}/>
             </div>
             <div className='flex flex-col items-start'>
               <h3 className="text-md pt-2 font-display"> Password </h3>
-              <input className="bg-[#ad4c4c] rounded-sm w-full "></input>
+              <input className="bg-[#ad4c4c] rounded-sm w-full" type="password" onChange={e => setPassword(e.target.value)}/>
             </div>
             <button className='p-3 bg-[#ad4c4c] rounded-sm border mx-auto my-6 border-black text-white font-display hover:bg-[#ad4c5c] cursor-pointer' onClick={onSubmitPress}>Submit</button>
+            <button className='p-3 bg-[#ad4c4c] rounded-sm border mx-auto my-6 border-black text-white font-display hover:bg-[#ad4c5c] cursor-pointer' onClick={onSignUp}>Sign Up</button>
           </div>
         </div>
         </main>
