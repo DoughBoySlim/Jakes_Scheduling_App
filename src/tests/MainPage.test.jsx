88860c459434c85ced065349763b92136c64ef54
DoughBoySlim <rx5857yk@go.minnstate.edu>
2025-07-14 16:11:49 -0500

    installing testing packages for unit/integration testing

---
+++ src/tests/MainPage.test.jsx
@@ -0,0 +1,6 @@
+import MainPage from "../components/MainPage";
+import { render, screen } from '@testing-library/react'
+import userEvent from "@testing-library/user-event";
+import '@testing-library/jest-dom'
+
+
