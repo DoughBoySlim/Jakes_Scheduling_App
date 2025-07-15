90de3481a8fedae970d9e7c1956d4549416f1195
DoughBoySlim <rx5857yk@go.minnstate.edu>
2025-07-15 15:36:16 -0500

    Updated Calendar.jsx with supabase connections to get fullname and to get data to create an event. I need to decide how to build the calendar. before deciding how to continue

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import supabase from '../config/supabaseClient'
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import { format, parse, startOfWeek, getDay, startOfDay } from 'date-fns'
+import { format, parse, startOfWeek, getDay, startOfDay, differenceInCalendarDays } from 'date-fns'
 import { enUS } from 'date-fns/locale/en-US'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+// Supabase Connection To DB
+
+// Getting shift data to put into events
+async function getShiftData() {
+    const { data, error } = await supabase
+    .from('shifts')
+    .select(
+        'employee_id',
+        'shift_date',
+        'start_time',
+        'end_time',
+        'employee_type'
+    ) 
+
+    return { data, error };
+}
+
+
+// Getting A User's Name to print into the Calendar for others to see:
+
+async function getUserFullName(id) {
+    const { data, error } = await supabase
+    .from('users')
+    .select('full_name')
+    .eq('employee_id', id)
+    .single();
+
+    return { data, error };
+}
+
 
 const locales = {
     'en-US': enUS,
@@ -17,11 +49,17 @@ const localizer = dateFnsLocalizer({
     locales,
 });
 
+// Hardcoded Events
 const events = [
     {
         title: 'Manager',
         start: new Date(2025, 6, 15, 10, 0),
         end: new Date(2025, 6, 15, 14, 0),
+    },
+    {
+        title: 'Cook',
+        start: new Date(2025, 6, 16, 10, 0),
+        end: new Date(2025, 6, 16, 14, 0),
     },
 ];
 
@@ -31,8 +69,23 @@ const maxTime = new Date();
 maxTime.setHours(21,0,0,0);
 
 export default function MyCalendar() {
+
+    const[calendarView, setCalendarView] = useState(() =>
+        window.innerWidth < 768 ? 'day' : 'week'
+    );
+
+    useEffect(() => {
+        function handleResize() {
+            window.innerWidth < 768 ? setCalendarView('day') : setCalendarView('week');
+        }
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+
     return (
-        <div className="p-4">
+        <div className="p-4 m-10">
             <h2 className="text-xl font-semibold mb-4">My Calendar</h2>
             <Calendar
                 localizer={localizer}
@@ -40,8 +93,8 @@ export default function MyCalendar() {
                 startAccessor="start"
                 endAccessor="end"
                 style={{ height: 600 }}
-                defaultView="week"
-                views={['week']}
+                defaultView={calendarView}
+                views={[calendarView]}
                 min={minTime}
                 max={maxTime}
             />
